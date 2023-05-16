ea268b402aa7cc63a1d4ff8cdd290ea67474cfac
Wartuu <88190400+Wartuu@users.noreply.github.com>
2023-05-16 17:45:13 +0200

    Create polish.h

---
+++ polish.h
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string.h>
+#include <math.h>
+
+
+// logical
+
+#define jezeli if
+#define zwrot return
+#define dopoki while
+#define prawda true
+#define falsz false;
+#define lub ||
+#define oraz &&
+#define domyslne default
+#define przerwij break
+#define kontynuowac continue
+
+#define przelacznik switch
+#define przypadek case
+#define to this
+
+
+
+// object
+
+#define klasa class
+#define publiczne public
+#define prywatne private
+#define chronione protected
+#define szablon template
+#define nowy new
+#define wirtualna virtual
+#define statyczna static
+
+// default values
+
+#define znak char
+#define cyfra int
+#define dluga long
+#define krotka short
+#define pustka void
+
+#define bez_znaku unsigned
+#define ze_znakiem signed
+
+
+// std, math base
+
+#define wypisz std::cout
+#define wczytaj std::cin
+#define potega std::pow
+#define pierwiastek
+#define pi 3.14159265359
+#define tekst std::string
+#define koniec_lini std::endl
